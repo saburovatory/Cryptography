3b2f56405928a52a5ced103394c7fbf9eec24c45
saburovatory <saburovatory@yandex.ru>
2017-01-05 16:07:04 +0300

    Структурирован код, латинизирован вывод

--- diploma/Firstone.cpp
+++ diploma/Firstone.cpp
@@ -9,6 +9,8 @@
 
 using namespace std;
 
+#pragma region Files
+
 #define INPUT "input.txt"
 #define LETTERS "input_only_letters.txt"
 #define LETTERS_BYTE "letters_byte.txt"
@@ -19,6 +21,8 @@ using namespace std;
 #define INPUT_DECRYPT "input_decrypt.txt"
 #define KEY "key.txt"
 
+#pragma endregion
+
 struct cmp_str
 {
 	bool operator()(char const *a, char const *b) const
@@ -27,6 +31,7 @@ struct cmp_str
 	}
 };
 
+//РСЛОС регистр сдвига с линейной обратной связью 
 int LFSR(void)
 {
 	//static unsigned long S = 0x00000001;
@@ -37,33 +42,17 @@ int LFSR(void)
 	return S & 0x00000001;
 }
 
-void create_gystogram(map<char, int> map) {
-
-}
-
-int main()
-{
-    setlocale(0, "RUS");
-
+int removing_symbols() {
 	ifstream f_input;
-	ofstream f_letters, f_count1, f_count2, f_crypt_byte, f_decrypt, f_key, f_crypt, f_letters_byte;
+	ofstream f_letters, f_letters_byte;
 
 	f_input.open(INPUT);
 	f_letters_byte.open(LETTERS_BYTE);
 	f_letters.open(LETTERS);
-	f_count1.open(SYMBOL1);
-	f_count2.open(SYMBOL2);
-	f_crypt.open(INPUT_CRYPT);
-	f_crypt_byte.open(INPUT_CRYPT_BYTE);
-	f_decrypt.open(INPUT_DECRYPT);
-	f_key.open(KEY);
 
 	char symbol;
 	int symbols_count = 0;
 	bitset<8> symbol_byte;
-	long start = clock();
-    cout << "Удаление лишних символов и преобразование в двоичную систему: ";
-
 	while ((symbol = f_input.get()) != -1)
 		if (symbol <= 'Z' && symbol >= 'A' || symbol <= 'z' && symbol >= 'a' || symbol == ' ')
 		{
@@ -72,61 +61,64 @@ int main()
 			f_letters_byte << symbol_byte;
 			f_letters << symbol;
 		}
-	long finish = clock();
-	cout << (finish - start) / 1000.0 << " c" << endl;
-
 	symbols_count *= 8;
 
 	f_input.close();
 	f_letters.close();
 	f_letters_byte.close();
 
-	ifstream ifs(LETTERS);
+	return symbols_count;
+}
+
+void counting_symbols() {
+	ifstream f_letters;
+	ofstream f_count1, f_count2;
+
+	f_letters.open(LETTERS);
+	f_count1.open(SYMBOL1);
+	f_count2.open(SYMBOL2);
 
 	char symbol1, symbol2;
 	map<char, int> mp1;
 	map<char*, int, cmp_str> mp2;
 
-	cout << "Подсчет одиночных и парных символов: ";
-	start = clock();
-
-    char sec[3];
-	while (ifs.get(symbol1) && ifs.get(symbol2))
+	char sec[3];
+	while (f_letters.get(symbol1) && f_letters.get(symbol2))
 	{
-        //char *sec = (char *)malloc(3);
 		sec[0] = symbol1;
 		sec[1] = symbol2;
 		sec[2] = 0;
-		ifs.seekg(-1, ios_base::cur);
+		f_letters.seekg(-1, ios_base::cur);
 		mp1[symbol1]++;
 		mp2[sec]++;
 	}
-	finish = clock();
-	cout << (finish - start) / 1000.0 << " c" << endl;
-	create_gystogram(mp1);
 
 	for (map<char, int>::iterator p = mp1.begin(); p != mp1.end(); ++p) // проходим по всем элементам 
-		f_count1<<p->first <<':'<<p->second <<endl;
-	f_count1.close();
-
+		f_count1 << p->first << ':' << p->second << endl;
+	
 	for (map<char*, int>::iterator p1 = mp2.begin(); p1 != mp2.end(); ++p1)
-		f_count2<<p1->first<<' '<<p1->second<<endl;
+		f_count2 << p1->first << ' ' << p1->second << endl;
+
+	f_letters.close();
+	f_count1.close();
 	f_count2.close();
+}
+
+void encryption(int symbols_count) {
+	ifstream f_letters_byte;
+	ofstream f_crypt_byte, f_key, f_crypt;
+	char s[8];
+
+	f_letters_byte.open(LETTERS_BYTE);
+	f_crypt.open(INPUT_CRYPT);
+	f_crypt_byte.open(INPUT_CRYPT_BYTE);
+	f_key.open(KEY);
 
 	for (int i = 0; i < 32; i++)
 		LFSR();
 
-	ifs.close();
-
-	ifs.open(LETTERS_BYTE);
-
-	char s[8];
-
-	cout << "Создание ключа и шифрование: ";
-	start = clock();
-
 	for (int i = 1; i <= symbols_count; i++) {
-		int byte = LFSR(), symbol = ifs.get();
+		int byte = LFSR(), symbol = f_letters_byte.get();
 		f_key << byte;
 		f_crypt_byte << (byte ^ (symbol - '0'));
 		s[7 - ((i - 1) % 8)] = (byte ^ (symbol - '0'));
@@ -137,19 +129,24 @@ int main()
 			f_crypt << (char)symbol;
 		}
 	}
-	finish = clock();
-	cout << (finish - start) / 1000.0 << " c" << endl;
 
-	ifs.close();
+	f_letters_byte.close();
 	f_crypt_byte.close();
 	f_key.close();
+	f_crypt.close();
+}
 
-	ifstream inf_1(INPUT_CRYPT_BYTE), inf_2(KEY);
-	
-	cout << "Дешифрование: ";
-	start = clock();
+void decryption(int symbols_count) {
+	ofstream f_decrypt;
+	ifstream input_crypt_byte, key;
+	char s[8];
+
+	f_decrypt.open(INPUT_DECRYPT);
+	input_crypt_byte.open(INPUT_CRYPT_BYTE);
+	key.open(KEY);
+
 	for (int i = 1; i <= symbols_count; i++) {
-		s[7 - ((i-1) % 8)] = ((inf_2.get() - '0') ^ (inf_1.get() - '0'));
+		s[7 - ((i - 1) % 8)] = ((key.get() - '0') ^ (input_crypt_byte.get() - '0'));
 		if (i % 8 == 0) {
 			int symbol = 0;
 			for (int j = 7; j >= 0; j--) {
@@ -158,11 +155,40 @@ int main()
 			f_decrypt << (char)symbol;
 		}
 	}
-	finish = clock();
-	cout << (finish - start) / 1000.0 << " c" << endl;
+
 	f_decrypt.close();
-	inf_1.close();
-	inf_2.close();
+	input_crypt_byte.close();
+	key.close();
+}
+
+int main()
+{
+	long start = clock();
+    cout << "Removing of extra characters and converting to a binary system: ";
+	int symbols_count = removing_symbols();
+	long finish = clock();
+	cout << (finish - start) / 1000.0 << " s" << endl;
+
+
+	cout << "Counting single and double symbols: ";
+	start = clock();
+	counting_symbols();
+	finish = clock();
+	cout << (finish - start) / 1000.0 << " s" << endl;
+	
+
+	cout << "Creating key and encrypting: ";
+	start = clock();
+	encryption(symbols_count);
+	finish = clock();
+	cout << (finish - start) / 1000.0 << " s" << endl;
+
+
+	cout << "Decrypting: ";
+	start = clock();
+	decryption(symbols_count);
+	finish = clock();
+	cout << (finish - start) / 1000.0 << " s" << endl;
 
 	system("pause");
 	return 0;
