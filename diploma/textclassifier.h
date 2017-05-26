e1da3436c2e2db3c55a5b50cc0e92baac12ca264
ioannkozlitin <ioannkozlitin@gmail.com>
2017-05-26 12:44:14 +0300

    Добавил описание нового алгортма и заготовку класса для его реализации

---
+++ diploma/textclassifier.h
@@ -0,0 +1,32 @@
+#ifndef TEXTCLASSIFIER_H
+#define TEXTCLASSIFIER_H
+
+#include <map>
+#include <string>
+
+class TextClassifier
+{
+public:
+    struct resultType
+    {
+        int classNumber = 0;
+        int symbolsToRecognize = 0;
+        double probability = 0;
+    };
+
+    resultType Process(const std::string &example, int answer);
+
+private:
+
+    //Хранилище для статистики открытых текстов
+    std::map<std::string, int> _openText;
+
+    //Хранилище для статистики шифрованных текстов
+    std::map<std::string, int> _encryptText;
+
+    //Высота дерева, она же размер слова, т.е. 2 - биграммы, 3 - триграммы и т.п.
+    //Это длинна подстроки, которую надо подставлять в качестве аргумента в _openText и _encryptText;
+    int _wordSize = 3;
+};
+
+#endif // TEXTCLASSIFIER_H
